97d35602118f3a51564a9a87dd9f6c7a2d16e6c9
Marcio Mortarino <mmortarino@ar0fvfxm0qshv2j.ml.com>
2019-01-11 17:18:01 -0300

    Se quitan los botones y se agrega un pickerView

--- MyAppOne/MyAppOne/VC/Initial/MAOInitialViewController.m
+++ MyAppOne/MyAppOne/VC/Initial/MAOInitialViewController.m
@@ -11,9 +11,12 @@
 #import "MAOInitialViewService.h"
 #import "MAOListViewController.h"
 
-@interface MAOInitialViewController ()
+@interface MAOInitialViewController ()<UIPickerViewDelegate, UIPickerViewDataSource >
 
 @property (weak, nonatomic) IBOutlet UIActivityIndicatorView *MAOProgressIndicator;
+@property (weak, nonatomic) IBOutlet UIPickerView *pickerView;
+@property IBOutlet NSArray *pickerData;
+@property IBOutlet NSString *selectedItem;
 
 @end
 
@@ -22,42 +25,12 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self hideProgressIndicator];
+    
+    _pickerData = @[@"Cargar datos ordenados asc", @"Cargar datos ordenados desc",
+                    @"Cargar datos por fecha", @"Cargar datos ordenados por ID"];
+    _pickerView.delegate = self;
+    _pickerView.dataSource = self;
 }
-
-- (IBAction)onClickSelection:(id)sender {
-    [self showProgressIndicator];
-    [[MAOInitialViewService sharedInstance] fetchItunesDataWithCompletionBlock:^(NSArray *array, NSError *error) {
-        if (!error) {
-            array = [array valueForKey:@"results"];
-            NSMutableArray *results = [[NSMutableArray alloc] init];
-            for (NSDictionary *itemModel in array) {
-                [results addObject: [MAOListViewControllerModel obtainItemsFromDicionary: itemModel]] ;
-            }
-            
-            if ([[sender currentTitle]  isEqual: @"Cargar datos ordenados por fecha"]) {
-                [self sortArrays:results by:@"releaseDate"];
-            } else if ([[sender currentTitle]  isEqual: @"Cargar datos ordenados desc"]) {
-                [self sortArrays:results by:@"desc"];
-            } else if ([[sender currentTitle]  isEqual: @"Cargar datos ordenados por ID"]) {
-                [self sortArrays:results by:@"trackId"];
-            } else {
-                [self sortArrays:results by:@""];
-            }
-        } else {
-            UIAlertController* alert = [UIAlertController alertControllerWithTitle:@"Error Message"
-                                                                           message:@"Oh! Something went wrong, please press again"
-                                                                    preferredStyle:UIAlertControllerStyleAlert];
-            
-            UIAlertAction* defaultAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault
-                                                                  handler:^(UIAlertAction * action) {}];
-            
-            [alert addAction:defaultAction];
-            [self presentViewController:alert animated:YES completion:nil];
-        }
-        [self hideProgressIndicator];
-    }];
-}
-
 
 -(void)sortArrays:(NSMutableArray *)array by:(NSString *)order {
     NSArray *arrayTemp = [array sortedArrayUsingComparator:
@@ -72,7 +45,6 @@
                     return [obj1.trackName compare:obj2.trackName];
                  }
     }];
-    
     MAOListViewController *listViewControllerPtr = [[MAOListViewController alloc] init];
     [self.navigationController pushViewController:[listViewControllerPtr initWithModel:arrayTemp] animated:YES];
 }
@@ -85,6 +57,52 @@
 -(void)hideProgressIndicator{
     [self.MAOProgressIndicator setHidden:YES];
     [self.MAOProgressIndicator stopAnimating];
+}
+
+- (NSInteger)numberOfComponentsInPickerView:(nonnull UIPickerView *)pickerView {
+    return 1;
+}
+
+- (NSInteger)pickerView:(nonnull UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component {
+    return _pickerData.count;
+}
+
+- (NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component {
+    return _pickerData[row];
+}
+
+- (void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component {
+    [self showProgressIndicator];
+    [[MAOInitialViewService sharedInstance] fetchItunesDataWithCompletionBlock:^(NSArray *array, NSError *error) {
+        if (!error) {
+            array = [array valueForKey:@"results"];
+            NSMutableArray *results = [[NSMutableArray alloc] init];
+            for (NSDictionary *itemModel in array) {
+                [results addObject: [MAOListViewControllerModel obtainItemsFromDicionary: itemModel]] ;
+            }
+
+            if ([self.pickerData[row] isEqual: @"Cargar datos ordenados por fecha"]) {
+                [self sortArrays:results by:@"releaseDate"];
+            } else if ([self.pickerData[row]  isEqual: @"Cargar datos ordenados desc"]) {
+                [self sortArrays:results by:@"desc"];
+            } else if ([self.pickerData[row]  isEqual: @"Cargar datos ordenados por ID"]) {
+                [self sortArrays:results by:@"trackId"];
+            } else {
+                [self sortArrays:results by:@""];
+            }
+        } else {
+            UIAlertController* alert = [UIAlertController alertControllerWithTitle:@"Error Message"
+                                                                           message:@"Oh! Something went wrong, please press again"
+                                                                    preferredStyle:UIAlertControllerStyleAlert];
+
+            UIAlertAction* defaultAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault
+                                                                  handler:^(UIAlertAction * action) {}];
+
+            [alert addAction:defaultAction];
+            [self presentViewController:alert animated:YES completion:nil];
+        }
+        [self hideProgressIndicator];
+    }];
 }
 
 @end
