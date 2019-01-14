9a56c9d7969d84a5c2fd6f000f8e09eba7000b4f
Gonzalo <quaringonzalo@gmail.com>
2019-01-14 16:01:31 -0300

    Refactorizo manejo de bloques para ordenamiento

---
+++ MyAppOne/MyAppOne/VC/Details/songDetailXIBView.h
@@ -0,0 +1,17 @@
+//
+//  songDetailXIBView.h
+//  MyAppOne
+//
+//  Created by Gonzalo Alexis Quarin on 12/01/2019.
+//  Copyright © 2019 iOS-accelerator. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface songDetailXIBView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MyAppOne/MyAppOne/VC/Details/songDetailXIBView.m
@@ -0,0 +1,21 @@
+//
+//  songDetailXIBView.m
+//  MyAppOne
+//
+//  Created by Gonzalo Alexis Quarin on 12/01/2019.
+//  Copyright © 2019 iOS-accelerator. All rights reserved.
+//
+
+#import "songDetailXIBView.h"
+
+@implementation songDetailXIBView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
